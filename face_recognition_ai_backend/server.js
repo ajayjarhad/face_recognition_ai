bb9bed8f1ca7d44257992a35dcbde9b55f521982
Ajay Jarhad <ajayjarhad1@gmail.com>
2020-06-26 07:59:25 +0530

    Backend connected with the frontend and /login is working now

--- face_recognition_ai_backend/server.js
+++ face_recognition_ai_backend/server.js
@@ -1,11 +1,95 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const cors  = require('cors')
 const app = express()
+const saltRounds = 10;
+app.use(bodyParser.json())
+app.use(cors())
+const database = {
+    users : [
+        {
+        id : '1',
+        name: 'Ajay',
+        email: 'ajay@email.com',
+        password: 'pass',
+        entries: 0,
+        joined: new Date()
+    },
+    {
+        id : '2',
+        name: 'Bunny',
+        email: 'bunny@email.com',
+        password: 'bunnyTheCoolest',
+        entries: 0,
+        joined: new Date()
+    }
+]
+}
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Working<h1>')
+    res.send(database.users)
 })
 
-app.listen(3000,()=>{
-    console.log('Connected on port 3000.')
+app.post('/signin',(req,res)=>{
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+        res.json('Success')
+    }else {
+            res.json('Error Logging In')
+        }
+})
+
+app.post('/register',(req,res)=>{
+    const {name, email, password} = req.body;
+
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        console.log(hash)
+    });
+
+    database.users.push(
+        {
+        id : '3',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id',(req, res)=>{
+    console.log(req.params)
+    const {id} = req.params
+    let found = false
+    database.users.forEach(user=>{
+        if(user.id === id){
+            res.json(user)
+            found = true
+        }
+        
+    })
+    if(!found){
+        res.json('Cannot fetch a thing')
+    }
+})
+
+app.put('/image',(req,res)=>{
+    const {id} = req.body
+    let found = false
+    database.users.forEach(user=>{
+        if(user.id === id){
+            user.entries++
+            found = true
+            return res.json(user.entries)
+            
+        }
+    })
+    if(!found){
+        res.json(`Such user doesn't exist`)
+    }
+})
+
+app.listen(3001,()=>{
+    console.log('Connected on port 3001.')
 })
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,33 +1,67 @@
 import React from 'react'
 
-const Signin = ({onRouteChange}) =>{
-    return(
-        <article className="br3 ba dark-gray b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className="pa4 black-80 white">
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f1 fw6 ph0 mh0 ">Sign In</legend>
-                    <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
-                    </div>
-                    <div className="mv3">
-                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-                    </div>
-                    </ fieldset>
-                    <div className="">
-                    <input className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib white" type="submit" value="Sign in" 
-                    onClick={()=>onRouteChange('home')}
-                    />
-                    </div>
-                    <div className="lh-copy mt3">
-                    <p onClick={()=> onRouteChange('register')} className="f6 link dim white db pointer">Register</p>
-                    </div>
-                </div>
-            </main>
-        </article>
-    )
+class Signin extends React.Component {
+constructor(props) {
+    super(props)
+    this.state={
+        signInEmail: '',
+        signInPassword: ''
+    }
+    }
+
+onSignInEmail=(event)=>{
+this.setState({signInEmail: event.target.value})
 }
+onSignInPassword=(event)=>{
+    this.setState({signInPassword:event.target.value})
+}
+onSubmit=()=>{
+   fetch('http://localhost:3001/signin',{
+       method: 'POST',
+       headers:{'Content-Type': 'application/json'},
+       body: JSON.stringify({
+           email: this.state.signInEmail,
+           password:this.state.signInPassword
+       })
+   })
+   .then(response=>response.json())
+   .then(data =>{
+       if(data === 'Success') {
+           this.props.onRouteChange('home')
+       }
+   })
+    
+}
+    render() {
+        return(
+            <article className="br3 ba dark-gray b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <main className="pa4 black-80 white">
+                    <div className="measure">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="f1 fw6 ph0 mh0 " >Sign In</legend>
+                        <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address" >Email</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" onChange={this.onSignInEmail}/>
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6" htmlFor="password" >Password</label>
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" onChange={this.onSignInPassword} />
+                        </div>
+                        </ fieldset>
+                        <div className="">
+                        <input className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib white" type="submit" value="Sign in" 
+                        onClick={this.onSubmit}
+                        />
+                        </div>
+                        <div className="lh-copy mt3">
+                        <p  className="f6 link dim white db pointer">Register</p>
+                        </div>
+                    </div>
+                </main>
+            </article>
+        )
+    }
+    }
+ 
 
 export default Signin
