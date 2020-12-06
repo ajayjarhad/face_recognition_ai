2ec0e49e8a3d75188c4174a0f66de3bd7b1b0a47
Ajay Jarhad <ajayjarhad1@gmail.com>
2020-12-06 20:35:40 +0530

    Added on keyPress event

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -38,6 +38,17 @@ saveAuthTokenInSessions = (token) => {
       })
       .catch(console.log)
   }
+
+    enterKey = (event) => {
+        if (event.key === 'Enter') {
+            
+            console.log('enter press here! ')
+            this.onSubmit();
+            
+       
+    }
+}
+    
     render() {
         return(
             <article className="br3 ba dark-gray b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -51,16 +62,17 @@ saveAuthTokenInSessions = (token) => {
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password" >Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" onChange={this.onSignInPassword} />
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" onChange={this.onSignInPassword}
+                                 onKeyPress={this.enterKey}/>
                         </div>
                         </ fieldset>
                         <div className="">
-                        <input className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib white" type="submit" value="Sign in" 
-                        onClick={this.onSubmit}
-                        />
+                            <input className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib white" type="submit" value="Sign in"
+                                onClick={this.onSubmit}
+                               />
                         </div>
                         <div className="lh-copy mt3">
-                        <p  onClick={() => this.props.onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+                        <p  onClick={() => this.props.onRouteChange('register')} className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib white">Register</p>
                         </div>
                     </div>
                 </main>
